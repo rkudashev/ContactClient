52427366a35a2152bd7ca7690a259bc55b439f12
rkudashev <kudashev.ravil@yandex.ru>
2026-04-21 16:35:01 +0500

    Создал структуру документа.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя контакта</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Имя1</td>
+                <td>example@gector.ru</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
