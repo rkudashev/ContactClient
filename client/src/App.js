4c0e27e20b4bf74864f1c44bfddea483a14b5076
rkudashev <kudashev.ravil@yandex.ru>
2026-04-23 16:46:42 +0500

    Добавление новых запиисей через react хук.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
+import React, {useState} from "react";
 import TableContact from "./layout/TableContact/TableContact";
 
-const contacts = [
-  {id: 1, name: "Yasya1", email: "pac1@nigga.com"},
-  {id: 2, name: "Yasya2", email: "pac2@nigga.com"},
-  {id: 3, name: "Yasya3", email: "pac3@nigga.com"},
-  {id: 4, name: "Yasya4", email: "pac4@nigga.com"},
-  {id: 5, name: "Yasya5", email: "pac5@nigga.com"},
-  {id: 6, name: "Yasya6", email: "pac6@nigga.com"},
-]
+
 
 const App = () => {
+  const [contacts, setContacts] = useState(
+    [
+      {id: 1, name: "Yasya1", email: "pac1@nigga.com"},
+      {id: 2, name: "Yasya2", email: "pac2@nigga.com"},
+      {id: 3, name: "Yasya3", email: "pac3@nigga.com"},
+      {id: 4, name: "Yasya4", email: "pac4@nigga.com"},
+      {id: 5, name: "Yasya5", email: "pac5@nigga.com"},
+      {id: 6, name: "Yasya6", email: "pac6@nigga.com"},
+    ]
+  );
+  
+  const addContact = () => {
+    const item = {
+      id: 2,//contacts[-1].id + 1,
+      name: "Varlam",
+      email: "varlam@pec.ng"
+    };
+  
+    setContacts([...contacts, item]);
+    console.log(contacts);
+  };
+  
   return (
     <div className="container mt-5">
       <div className="card">
@@ -19,6 +35,12 @@ const App = () => {
 
         <div className="card-body">
           <TableContact contacts={contacts}/>
+          <div>
+            <button className="btn btn-primary"
+                    onClick={() => {addContact()}}>
+              Добавить контакт
+            </button>
+          </div>
         </div>
       </div>
     </div>
