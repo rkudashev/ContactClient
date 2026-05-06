9c52a62bc8699bfea8e47d7e0ba3517451eb7993
rkudashev <kudashev.ravil@yandex.ru>
2026-05-06 15:35:19 +0500

    Настроил CORS.

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,11 +9,19 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<DataContext>();
 builder.Services.AddSingleton<ContactStorage>();
 
+builder.Services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins("http://localhost:3000");
+}));
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
