e6afa9a660287b385bf801ff7fb91438d1d04647
rkudashev <kudashev.ravil@yandex.ru>
2026-06-02 12:14:57 +0500

    Добавил инициализацию БД фейковыми данными, если она пуста. Вынес этот функционал в Initializer. Также таблица contacts будет создана, если отсутствует. Все это происходит при старте программы.

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddServiceCollection(builder.Configuration);
 
 var app = builder.Build();
+app.Services.AddCustomService(builder.Configuration);
 
 app.UseSwagger();
 app.UseSwaggerUI();
---
+++ Api/Seed/FakerInitializer.cs
@@ -0,0 +1,60 @@
+using Api.Model;
+using Bogus;
+using Microsoft.Data.Sqlite;
+
+namespace Api.Seed;
+public class FakerInitializer : IInitializer
+{
+    private string connectionString;
+
+    public FakerInitializer(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public void Initialize()
+    {
+        using var connection = new SqliteConnection(connectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+
+        string sql = @"
+            CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL
+            )";
+
+        command.CommandText = sql;
+
+        command.ExecuteNonQuery();
+
+        command.CommandText = @"SELECT count(*) FROM contacts";
+
+        long count = (long)command.ExecuteScalar();
+
+        if(count == 0)
+        {
+            var faker = new Faker<Contact>("ru")
+                .RuleFor(c => c.Name, f => f.Name.FullName())
+                .RuleFor(c => c.Email, f => f.Internet.Email());
+
+            var contacts = faker.Generate(20);
+
+            foreach(var contact in contacts)
+            {
+                command.CommandText = @"
+                    INSERT INTO contacts (name, email)
+                    VALUES ($name, $email)
+                ";
+
+                command.Parameters.Clear();
+
+                command.Parameters.AddWithValue("$name", contact.Name);
+                command.Parameters.AddWithValue("$email", contact.Email);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
