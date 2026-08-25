516d6a6a1f1579a1eec1be8ac727b7c873853a17
rkudashev <kudashev.ravil@yandex.ru>
2026-08-25 16:31:06 +0500

    Добавил пагинацию списка контактов и увеличил количество фейковых данных.

--- Api/Seed/SqliteEfFakerInitializer.cs
+++ Api/Seed/SqliteEfFakerInitializer.cs
@@ -25,7 +25,7 @@ public class SqliteEfFakerInitializer : IInitializer
                 .RuleFor(c => c.Name, f => f.Name.FullName())
                 .RuleFor(c => c.Email, (f, c) => GenerateEmailForName(c.Name));
 
-            var contacts = faker.Generate(20);
+            var contacts = faker.Generate(200);
 
             context.Contacts.AddRange(contacts);
             context.SaveChanges();
